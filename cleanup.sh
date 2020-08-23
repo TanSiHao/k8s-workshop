740969f5afcdcc57103dcfda215368fe3ac97dc4
TanSiHao <tan.si.hao@gmail.com>
2020-08-23 15:01:00 +0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl label namespace production oneagent.dynatrace.com/instance-
 kubectl delete namespace dynatrace
+kubectl label namespace production oneagent.dynatrace.com/instance-
 kubectl delete pod --all -n production
